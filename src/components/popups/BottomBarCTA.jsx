b99137c3fbc6145b8a8198266a166c0ae6b6126a
Anil Suthar <aniesuthar@gmail.com>
2025-01-28 10:25:57 +0530

    Bottom bar CTA

--- src/components/popups/BottomBarCTA.jsx
+++ src/components/popups/BottomBarCTA.jsx
@@ -2,16 +2,19 @@
 import { cn } from '@/lib/utils'
 import React from 'react'
 import { Button } from '../ui/button';
+import { usePopup } from '@/contexts/PopupProvider';
+import OfferPopup from './Offer';
 
 export default function BottomBarCTA() {
 
     const [open, setOpen] = React.useState(false);
+    const {showPopup} = usePopup();
 
     return (
         <div className={cn('fixed inset-x-0 bg-primary text-primary-foreground bottom-0 shadow-md z-50 flex justify-center p-2 gap-6')}>
             <p className='flex flex-col lg:flex-row  justify-center items-center text-lg gap-2 lg:gap-6'>
                 Get premium care for your loved ones
-                <Button variant="secondary">Get your free consultation</Button>
+                <Button variant="secondary" onClick={()=>showPopup(<OfferPopup/>, 0)}>Get your free consultation</Button>
             </p>
             {/* <div className='bg-primary w-8 text-center content-center text-primary-foreground' onClick={() => setOpen(!open)}>
                 {open ?
